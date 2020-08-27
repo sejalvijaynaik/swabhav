f63469afcfd46a0e37523ee17f9035f7ddbdeeda
sejal naik <flippyvippy@gmail.com>
2020-08-27 21:57:11 +0530

    8/27/2020

---
+++ Struts/JSONEmployeeList/src/main/java/com/techlabs/actions/JSONAction.java
@@ -0,0 +1,30 @@
+package com.techlabs.actions;
+
+import java.util.List;
+import com.opensymphony.xwork2.ActionSupport;
+import com.techlabs.model.Employee;
+import com.techlabs.service.EmployeeService;
+
+public class JSONAction extends ActionSupport {
+
+	private List<Employee> employees;
+
+	@Override
+	public String execute() throws Exception {
+
+		EmployeeService employeeService = EmployeeService.getEmployeeService();
+		employees = employeeService.getEmployees();
+		System.out.println(employees.size());
+
+		return "success";
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+}
