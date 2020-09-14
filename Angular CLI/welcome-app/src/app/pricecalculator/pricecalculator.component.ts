4e7a12d0f6bbc23bf02615f4acaf2acc9d6ff935
sejal naik <flippyvippy@gmail.com>
2020-09-14 16:58:27 +0530

    9/14/2020

--- Angular CLI/welcome-app/src/app/pricecalculator/pricecalculator.component.ts
+++ Angular CLI/welcome-app/src/app/pricecalculator/pricecalculator.component.ts
@@ -7,12 +7,10 @@ import { Component } from '@angular/core';
 })
 export class pricecalculatorComponent{
 
-  price:string = "0";
-  rate:string = "0";
   cost:number = 0;
   
-  priceCalculate():void {
+  priceCalculate(price:string, rate:string):void {
     
-    this.cost = parseInt(this.price) - (parseInt(this.rate)/100 *parseInt(this.price));
+    this.cost = parseInt(price) - (parseInt(rate)/100 *parseInt(price));
   }
 }
